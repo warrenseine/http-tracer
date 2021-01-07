7209aded5b6595b5163b11b7330d1760cda4d839
Warren Seine <w.seine@criteo.com>
2021-01-07 23:08:43 +0100

    ✅ Run some random HTTP requests.
    These are the requests we'll try to capture. The 5000 and 5001 ports are
    opened by the application itself to serve the front-end code.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,20 +1,72 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace HttpTracer.Server
 {
-    public class Program
+    internal static class Program
     {
-        public static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            await Task.WhenAll(Task.Run(RunHttpRequests), CreateHostBuilder(args).Build().RunAsync());
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static readonly IReadOnlyList<string> Urls = new[]
+        {
+            "http://localhost:5000/css/app.css", // 200 OK
+            "https://localhost:5001/css/app.css", // 200 OK (https)
+            "http://localhost:5000/favicon.ico", // 200 OK (long)
+            "http://localhost:5000/not-found.html", // 404 Not Found
+            "http://localhost:5001/foo.jpg", // Refused
+        };
+
+        private static readonly Random Random = new Random();
+
+        private static async Task RunHttpRequests()
+        {
+            await Task.Delay(5000);
+
+            while (true)
+            {
+                var url = Urls[Random.Next(Urls.Count)];
+                var delay = Random.Next(400) + 1100;
+                await Task.Delay(delay);
+
+                await RunHttpRequest(url);
+            }
+        }
+
+        private static async Task RunHttpRequest(string url)
+        {
+            var uri = new Uri(url);
+            var message = new HttpRequestMessage(HttpMethod.Get, uri.PathAndQuery);
+
+            using var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            using var client = new HttpClient(httpClientHandler)
+            {
+                BaseAddress = new Uri(uri.GetLeftPart(UriPartial.Authority))
+            };
+
+            try
+            {
+                await client.SendAsync(message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
     }
 }
